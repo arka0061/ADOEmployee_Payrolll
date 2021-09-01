4a66259e90696b1b83cfccc9e1f615851865d242
Arka <arka.spectacular@gmail.com>
2021-09-01 23:02:22 +0530

    [Arka]Add:Welcome Message

---
+++ ADOEmployee_Payroll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADOEmployee_Payroll
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To ADO Employee Payroll Assignment");
+        }
+    }
+}
