4890d86af4ead16a57b663d703b3fe4b911a7361
Arka <arka.spectacular@gmail.com>
2021-09-03 18:46:09 +0530

    [Arka]Add:Employees By Date Range

--- Queries/SqlProcedureDateRangeQuery.sql
+++ Queries/SqlProcedureDateRangeQuery.sql
@@ -1,12 +1,14 @@
-Create or Alter procedure spDeleteEmployeeDetails
+ALTER TABLE employee_payroll
+ALTER COLUMN StartDate date;
+Select * from employee_payroll
+Create or Alter procedure spDateRangeEmployee
 (	
-	@Id int	
+	@StartDate date
 )
 As
 Begin Try
-Delete from  
-employee_payroll 
-where  Id=@Id
+SELECT * FROM employee_payroll 
+WHERE StartDate BETWEEN CAST('2021-02-20' AS DATE ) AND '2021-05-20'
 End Try
 BEGIN CATCH
   SELECT
